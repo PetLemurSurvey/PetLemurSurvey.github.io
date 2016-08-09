f95feea8152a932ced81155365fd4c92aa9e3eaf
Toby <toby.schaeffer@gmail.com>
2016-08-09 14:14:04 +0300

    separated kvs to separate file.  added decades, full month field for table

--- js/index.js
+++ js/index.js
@@ -1,58 +1,18 @@
-var admin2_KV, 
-		admin1_KV,
-		lemur_category_KV,
-		row1_height,
+var row1_height,
 		row2_height,
 		map_height,
 		map_width,
 		item_width,
 		rowChartBarColor;
 
-
 // d3.json('https://petlemursurvey.herokuapp.com/', function(error, data) {	
 d3.json('http://localhost:3000/', function(error, data) {		
-	month_KV_digit = {
-		"no_response":"No response",
-		"i_dont_know":"I dont know",
-		"january":"1","february":"2","march":"3","april":"4","may":"5","june":"6","july":"7","august":"8","september":"9","october":"10","november":"11","december":"12"
-	};
-		month_KV_abbr = {
-		"no_response":"No response",
-		"i_dont_know":"I dont know",
-		"january":"Jan","february":"Feb","march":"Mar","april":"Apr","may":"May","june":"Jun","july":"July","august":"Aug","september":"Sept","october":"Oct","november":"Nov","december":"Dec"
-	};
+	// month_KV_digit = {
+	// 	"no_response":"No response",
+	// 	"i_dont_know":"I dont know",
+	// 	"january":"1","february":"2","march":"3","april":"4","may":"5","june":"6","july":"7","august":"8","september":"9","october":"10","november":"11","december":"12"
+	// };
 
-	decade_KV = {
-		"no_response":"No response",
-		"i_dont_know":"I don't know",
-		"1960's":"1960's","1970's":"1970's","1980's":"1980's","1990's":"1990's","2000's":"2000's",
-		"2010":"2010","2011":"2011","2012":"2012","2013":"2013","2014":"2014","2015":"2015","2016":"2016",
-		"before_1960":"before 1960"
-	};
-
-	year_KV = {
-		"no_response":"No response",
-		"i_dont_know":"I don't know",
-		"2016":"2016","2015":"2015","2014":"2014","2013":"2013","2012":"2012","2011":"2011","2010":"2010","2009":"2009","2008":"2008","2007":"2007","2006":"2006","2005":"2005","2004":"2004","2003":"2003","2002":"2002","2001":"2001","2000":"2000","1999":"1999","1998":"1998","1997":"1997","1996":"1996","1995":"1995","1994":"1994","1993":"1993","1992":"1992","1991":"1991","1990":"1990","1989":"1989","1988":"1988","1987":"1987","1986":"1986","1985":"1985","1984":"1984","1983":"1983","1982":"1982","1981":"1981","1980":"1980","1979":"1979","1978":"1978","1977":"1977","1976":"1976","1975":"1975","1974":"1974","1973":"1973","1972":"1972","1971":"1971","1970":"1970","1969":"1969","1968":"1968","1967":"1967","1966":"1966","1965":"1965","1964":"1964","1963":"1963","1962":"1962","1961":"1961","1960":"1960",
-		// ,
-		"before_1960":"before 1960"
-	};
-
-	quantity_KV = {
-		"no_answer":"No response",
-		"i_don_t_know":"I don't know",
-		"1":"1","2":"2","3":"3","4":"4","5":"5","6":"6","7":"7","8":"8","9":"9","10":"10","11":"11","12":"12","13":"13","14":"14","15":"15","16":"16","17":"17","18":"18","19":"19","20":"20"
-		,"more_than_20":"21 or more"
-	};
-	admin1_KV = {
-		"antananarivo":"Antananarivo","antsiranana":"Antsiranana","fianarantsoa":"Fianarantsoa","mahajanga":"Mahajanga","toamasina":"Toamasina","toliara":"Toliara","other":"Other","no_response":"No response"
-	};
-	admin2_KV = {
-		"alaotra-mangoro":"Alaotra-Mangoro","amoron.i_mania":"Amoron'I Mania","analamanga":"Analamanga","analanjirofo":"Analanjirofo","androy":"Androy","anosy":"Anosy","atsimo-andrefana":"Atsimo-Andrefana","atsimo-atsinanana":"Atsimo-Atsinanana","atsinanana":"Atsinanana","betsiboka":"Betsiboka","boeny":"Boeny","bongolava":"Bongolava","diana":"Diana","haute_matsiatra":"Haute Matsiatra","ihorombe":"Ihorombe","itasy":"Itasy","melaky":"Melaky","menabe":"Menabe","sava":"Sava","sofia":"Sofia","vakinankaratra":"Vakinankaratra","vatovavy-fitovinany":"Vatovavy-Fitovinany","no_response":"No response","other":"Other"
-	};
-	lemur_category_KV = {
-		"aye_aye":"Aye Aye","bamboo_lemur":"Bamboo lemur","brown_lemur":"Brown lemur","dwarf_lemur":"Dwarf lemur","fork_marked_lemur":"Fork marked lemur","giant_mouse_lemurs":"Giant mouse lemurs","greater_bamboo_lemur":"Greater bamboo lemur","indri":"Indri","mouse_lemur":"Mouse lemur","ring-tailed_lemur":"Ring-tailed lemur","ruffed_lemurs":"Ruffed lemurs","sifaka":"Sifaka","sportive_lemur":"Sportive lemur","woolly_lemur":"Woolly lemur","I_dont_remembe":"I don't remember","no_response":"No response","other":"Other"
-	};
 	var lemurData = data.responses;
 	var fullDateFormat = d3.time.format('%Y-%m-%d');
 	var yearFormat = d3.time.format('%Y');
@@ -64,7 +24,8 @@ d3.json('http://localhost:3000/', function(error, data) {
 		d.lemurs_quantity = quantity_KV[d.lemurs_quantity];
 		d.categoryName = lemur_category_KV[d.lemur_category];
 		d.month_abbr = month_KV_abbr[d.month];
-		d.month_digit= month_KV_digit[d.month];
+		d.month_full= month_KV_full[d.month];
+		// d.month_digit= month_KV_digit[d.month];
 		d.year = year_KV[d.year];
 		d.decade = decade_KV[d.decade];
 		// d.year = d.year;
@@ -82,7 +43,8 @@ d3.json('http://localhost:3000/', function(error, data) {
 			//dc.pluck:  short hand for same kind of anonymous function we used for yearDim
 			// monthDim = ndx.dimension(dc.pluck('when_seen_month')),
 			monthDim_abbr = ndx.dimension(function(d) {return d.month_abbr}),
-			monthDim_digit = ndx.dimension(function(d) {return d.month_digit}),
+			monthDim_full = ndx.dimension(function(d) {return d.month_full}),
+			// monthDim_digit = ndx.dimension(function(d) {return d.month_digit}),
 			decadeDim = ndx.dimension(function(d) {return d.decade}),
 			yearDim = ndx.dimension(function(d) {return d.year}),
 			categoryNameDim = ndx.dimension(function(d) {return d.categoryName;}),
@@ -97,7 +59,8 @@ d3.json('http://localhost:3000/', function(error, data) {
 	var countPerDecade = decadeDim.group().reduceCount(),
 			countPerYear = yearDim.group().reduceCount(),
 			countPerMonth_abbr = monthDim_abbr.group().reduceCount(),
-			countPerMonth_digit = monthDim_digit.group().reduceCount(),
+			countPerMonth_full = monthDim_full.group().reduceCount(),
+			// countPerMonth_digit = monthDim_digit.group().reduceCount(),
 			categoryGroup = categoryNameDim.group().reduceCount(),
 			admin1ChartGroup = admin1ChartDim.group().reduceCount(),
 			admin2ChartGroup = admin2ChartDim.group().reduceCount(),
@@ -245,7 +208,7 @@ d3.json('http://localhost:3000/', function(error, data) {
 				.group(function (d) { return 'dc.js insists on putting a row here so I remove it using js'; })
 				.size(100)
 				.columns([
-					function (d) { return d.month_abbr; },
+					function (d) { return d.month_full; },
 					function (d) { return d.year; },
 					function (d) { return d.categoryName; },
 					function (d) { return d.lemurs_quantity; },
---
+++ js/kv.js
@@ -0,0 +1,39 @@
+var decade_KV = {
+		"before_1961":"before 1961",
+		"i_dont_know":"I don't know",
+		"no_response":"No response",
+		"1960s":"1960's","1970s":"1970's","1980s":"1980's","1990s":"1990's","2000s":"2000's",
+		"2010":"2010","2011":"2011","2012":"2012","2013":"2013","2014":"2014","2015":"2015","2016":"2016"
+	},
+	year_KV = {
+		"before_1961":"before 1961",
+		"i_dont_know":"I don't know",
+		"no_response":"No response",
+		"2016":"2016","2015":"2015","2014":"2014","2013":"2013","2012":"2012","2011":"2011","2010":"2010","2009":"2009","2008":"2008","2007":"2007","2006":"2006","2005":"2005","2004":"2004","2003":"2003","2002":"2002","2001":"2001","2000":"2000","1999":"1999","1998":"1998","1997":"1997","1996":"1996","1995":"1995","1994":"1994","1993":"1993","1992":"1992","1991":"1991","1990":"1990","1989":"1989","1988":"1988","1987":"1987","1986":"1986","1985":"1985","1984":"1984","1983":"1983","1982":"1982","1981":"1981","1980":"1980","1979":"1979","1978":"1978","1977":"1977","1976":"1976","1975":"1975","1974":"1974","1973":"1973","1972":"1972","1971":"1971","1970":"1970","1969":"1969","1968":"1968","1967":"1967","1966":"1966","1965":"1965","1964":"1964","1963":"1963","1962":"1962","1961":"1961","1960":"1960"
+		// ,
+	},
+	month_KV_abbr = {
+		"no_response":"No response",
+		"i_dont_know":"I dont know",
+		"january":"Jan","february":"Feb","march":"Mar","april":"Apr","may":"May","june":"Jun","july":"July","august":"Aug","september":"Sept","october":"Oct","november":"Nov","december":"Dec"
+	},
+	month_KV_full = {
+		"no_response":"No response",
+		"i_dont_know":"I dont know",
+		"january":"January","february":"Febuary","march":"March","april":"April","may":"May","june":"June","july":"July","august":"August","september":"September","october":"October","november":"November","december":"December"
+	},
+	quantity_KV = {
+		"no_answer":"No response",
+		"i_don_t_know":"I don't know",
+		"1":"1","2":"2","3":"3","4":"4","5":"5","6":"6","7":"7","8":"8","9":"9","10":"10","11":"11","12":"12","13":"13","14":"14","15":"15","16":"16","17":"17","18":"18","19":"19","20":"20"
+		,"more_than_20":"21 or more"
+	},
+	admin1_KV = {
+		"antananarivo":"Antananarivo","antsiranana":"Antsiranana","fianarantsoa":"Fianarantsoa","mahajanga":"Mahajanga","toamasina":"Toamasina","toliara":"Toliara","other":"Other","no_response":"No response"
+	},
+	admin2_KV = {
+		"alaotra-mangoro":"Alaotra-Mangoro","amoron.i_mania":"Amoron'I Mania","analamanga":"Analamanga","analanjirofo":"Analanjirofo","androy":"Androy","anosy":"Anosy","atsimo-andrefana":"Atsimo-Andrefana","atsimo-atsinanana":"Atsimo-Atsinanana","atsinanana":"Atsinanana","betsiboka":"Betsiboka","boeny":"Boeny","bongolava":"Bongolava","diana":"Diana","haute_matsiatra":"Haute Matsiatra","ihorombe":"Ihorombe","itasy":"Itasy","melaky":"Melaky","menabe":"Menabe","sava":"Sava","sofia":"Sofia","vakinankaratra":"Vakinankaratra","vatovavy-fitovinany":"Vatovavy-Fitovinany","no_response":"No response","other":"Other"
+	},
+	lemur_category_KV = {
+		"aye_aye":"Aye Aye","bamboo_lemur":"Bamboo lemur","brown_lemur":"Brown lemur","dwarf_lemur":"Dwarf lemur","fork_marked_lemur":"Fork marked lemur","giant_mouse_lemurs":"Giant mouse lemurs","greater_bamboo_lemur":"Greater bamboo lemur","indri":"Indri","mouse_lemur":"Mouse lemur","ring-tailed_lemur":"Ring-tailed lemur","ruffed_lemurs":"Ruffed lemurs","sifaka":"Sifaka","sportive_lemur":"Sportive lemur","woolly_lemur":"Woolly lemur","I_dont_remembe":"I don't remember","no_response":"No response","other":"Other"
+	};
